8a833fa1bfcb1a4eb1c11ba498782b069a5becc6
fahadsohail07 <fahadsohail08@gmail.com>
2023-05-02 21:15:08 +0500

    Crud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [enteredText, setEnteredText] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const [enteredGender, setEnteredGender] = useState("");
+
+  const handleTextChange = (event) => {
+    setEnteredText(event.target.value);
+  };
+  const handleAgeChange = (event) => {
+    setEnteredAge(event.target.value);
+  };
+  const handleGenderChange = (event) => {
+    setEnteredGender(event.target.value);
+  };
+
+  const addButtonHandler = () => {
+    console.log(enteredText);
+    localStorage.setItem(Math.random(), [
+      enteredText,
+      enteredAge,
+      enteredGender,
+    ]);
+    setEnteredText("");
+    setEnteredAge("");
+    setEnteredGender("");
+  };
+
+  // const showButtonHandler =() => {
+  //   localStorage.getItem
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={enteredText}
+        onChange={handleTextChange}
+      ></input>
+      <label>Enter your name</label>
+      <input type="text" value={enteredAge} onChange={handleAgeChange}></input>
+      <label>Enter your age</label>
+      <input
+        type="text"
+        value={enteredGender}
+        onChange={handleGenderChange}
+      ></input>
+      <label>Enter your gender</label>
+      <button onClick={addButtonHandler}>Add</button>
+
+      {/* <button onClick={showButtonHandler}>Show</button> */}
     </div>
   );
-}
+};
 
 export default App;
